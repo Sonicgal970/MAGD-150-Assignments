a93f46d064de4f7fe51f832ac218d18b5846ebfc
Sonicgal970 <58006716+Sonicgal970@users.noreply.github.com>
2020-04-23 14:07:36 -0500

    Add files via upload

---
+++ s20magd150lab01_Sobieski_2020_04_21_16_35_52 (1)/sketch.js
@@ -0,0 +1,28 @@
+function setup() {
+  createCanvas(128,128);
+}
+
+function draw() {
+  background(255);
+	rectMode(RADIUS);
+	fill(125);
+	rect(30, 30, 120, 120);
+	rectMode(CENTER);
+	noFill();
+	rect(60, 50, 40, 90, 40, 40, 0, 0);
+  line(80, 30, 40, 30);
+	line(45, 40, 45, 80);
+	line(50, 40, 50, 80);
+	line(55, 40, 55, 80);
+	line(60, 40, 60, 80);
+	line(65, 40, 65, 80);
+	line(70, 40, 70, 80);
+	line(75, 40, 75, 80);
+	line(40, 45, 80, 45);
+	line(40, 50, 80, 50);
+	line(40, 55, 80, 55);
+	line(40, 60, 80, 60);
+	line(40, 65, 80, 65);
+	line(40, 70, 80, 70);
+	line(40, 75, 80, 75);
+}
